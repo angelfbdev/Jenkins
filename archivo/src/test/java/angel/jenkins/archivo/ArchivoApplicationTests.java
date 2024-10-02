e78ea681facf77dc14027ed9bb565d4ed89f875b
angelfbdev <82325226+angelfbdev@users.noreply.github.com>
2024-10-02 11:13:17 +0200

    Add files via upload

---
+++ archivo/src/test/java/angel/jenkins/archivo/ArchivoApplicationTests.java
@@ -0,0 +1,13 @@
+package angel.jenkins.archivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
